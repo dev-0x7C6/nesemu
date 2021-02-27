87aba2134527dd393022edbe9c8c52283e4297fb
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2021-02-27 17:48:32 +0100

    core: added simple lda program example.

--- instructions.hpp
+++ instructions.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <registers.hpp>
+#include <opcodes.hpp>
 
 namespace instruction {
 
 template <typename type>
 constexpr auto lda(registers::cpu &cpu, const type value) noexcept {
-    cpu.A.u8 += value;
+    cpu.A.u8 += static_cast<std::uint8_t>(value);
 }
 
 } // namespace instruction
--- main.cpp
+++ main.cpp
@@ -2,10 +2,43 @@
 #include <opcodes.hpp>
 #include <registers.hpp>
 
+#include <concepts>
+#include <functional>
+#include <ranges>
+#include <vector>
+
+constexpr auto simple_lda_program = {
+    std::byte{0xa9},
+    std::byte{0x01},
+    std::byte{0xa9},
+    std::byte{0x02},
+    std::byte{0xa9},
+    std::byte{0x04}};
+
+struct memory {
+    std::byte *ptr{nullptr};
+    constexpr auto fetch() noexcept { return *ptr++; };
+};
+
+constexpr auto process_single_instruction(registers::cpu &state, memory &mem) {
+    switch (mem.fetch()) {
+        case opcode::LDA:
+            instruction::lda(state, mem.fetch());
+    }
+}
+
+auto process(registers::cpu &cpu) {
+    std::vector<std::byte> bytes = simple_lda_program;
+
+    memory mem{bytes.data()};
+
+    process_single_instruction(cpu, mem);
+    process_single_instruction(cpu, mem);
+    process_single_instruction(cpu, mem);
+}
+
 auto main(int, char **) -> int {
     registers::cpu cpu;
-    instruction::lda(cpu, 1);
-    instruction::lda(cpu, 1);
-    instruction::lda(cpu, 1);
+    process(cpu);
     return cpu.A.i8;
 }
